2cb59e8e2577e38bffa413c1de203d8f016263ad
Elsie Oduor <eoatieno@gmail.com>
2024-01-22 12:28:35 +0300

    Progress day 2

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -153,8 +153,6 @@ const page = () => {
         </div>
       </div>
     </section>
-
-
     </>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -72,6 +72,9 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      boxShadow: {
+        yellow: '0 4px 6px -1px rgba(255, 255, 0, 0.1), 0 2px 4px -1px rgba(255, 255, 0, 0.06)',
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
