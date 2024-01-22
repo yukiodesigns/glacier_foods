b0187ec3c0487d7e61f3f6cfa990e47d12d59224
Elsie Oduor <eoatieno@gmail.com>
2024-01-22 22:46:38 +0300

    setup register

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,9 +1,69 @@
-import React from 'react'
+// pages/register.js
+import React from 'react';
 
 const page = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full">
+        <div>
+          <img
+            className="mx-auto h-12 w-auto"
+            src="https://www.svgrepo.com/show/301692/login.svg"
+            alt="Workflow"
+          />
+          <h2 className="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900">
+            Create a new account
+          </h2>
+        </div>
+        <form className="mt-8">
+          <div className="rounded-md shadow-sm">
+            <div>
+              <label>Name</label>
+              <input
+                aria-label="Name"
+                name="name"
+                type="text"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
+                placeholder="Name"
+              />
+            </div>
+            <div className="-mt-px">
+              <label>Email Address</label>
+              <input
+                aria-label="Email address"
+                name="email"
+                type="email"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
+                placeholder="Email address"
+              />
+            </div>
+            <div className="-mt-px">
+              <label>Password</label>
+              <input
+                aria-label="Password"
+                name="password"
+                type="password"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
+                placeholder="Password"
+              />
+            </div>
+          </div>
 
-export default page
+          <div className="mt-6">
+            <button
+              type="submit"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-800 transition duration-150 ease-in-out"
+            >
+              Create account
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default page;
